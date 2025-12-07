795736cfa034ec15d2fc37e04dcc9a5ce8a221c5
luca conconi <lconconi@ethz.ch>
2025-12-07 08:08:57 +0100

    feat: Add module for hyperparameter setup and command-line argument parsing.

--- hparams/set_up_hparams.py
+++ hparams/set_up_hparams.py
@@ -30,7 +30,7 @@ def add_train_args(parser):
     parser.add_argument("--steps_per_update_ema", type=int, default=10)
     parser.add_argument("--steps_per_log", type=int, default=10)
     parser.add_argument("--steps_per_eval", type=int, default=20)
-    parser.add_argument("--steps_per_sample", type=int, default=20)
+    parser.add_argument("--steps_per_sample", type=int, default=100)
     parser.add_argument("--steps_per_checkpoint", type=int, default=20)
     parser.add_argument("--train_steps", type=int, default=500)
     parser.add_argument("--show_samples", type=int, default=32)
